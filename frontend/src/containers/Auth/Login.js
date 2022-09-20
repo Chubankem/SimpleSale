4e7e9c49e6d5f23042075698405903b9d975eed6
Chubankem <changbangao123@gmail.com>
2022-09-20 21:01:27 +0700

    commit frontend
    commit fronttend

---
+++ frontend/src/containers/Auth/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.btnLogin = React.createRef();
+    }
+
+
+    signUpButton = () => {
+
+    };
+    signInButton = () => {
+
+    };
+
+    render() {
+        return (
+            <div class="container">
+                <div class="screen">
+                    <div class="screen__content">
+                        <form class="login">
+                            <div class="login__field">
+                                <i class="login__icon fas fa-user"></i>
+                                <input type="text" class="login__input" placeholder="User name / Email" />
+                            </div>
+                            <div class="login__field">
+                                <i class="login__icon fas fa-lock"></i>
+                                <input type="password" class="login__input" placeholder="Password" />
+                            </div>
+                            <button class="button login__submit">
+                                <span class="button__text">Log In Now</span>
+                                <i class="button__icon fas fa-chevron-right"></i>
+                            </button>
+                        </form>
+                        <div class="social-login">
+                            <h3>log in via</h3>
+                            <div class="social-icons">
+                                <a href="#" class="social-login__icon fab fa-instagram"></a>
+                                <a href="#" class="social-login__icon fab fa-facebook"></a>
+                                <a href="#" class="social-login__icon fab fa-twitter"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="screen__background">
+                        <span class="screen__background__shape screen__background__shape4"></span>
+                        <span class="screen__background__shape screen__background__shape3"></span>
+                        <span class="screen__background__shape screen__background__shape2"></span>
+                        <span class="screen__background__shape screen__background__shape1"></span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        lang: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
