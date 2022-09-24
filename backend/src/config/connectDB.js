128b5163e880fe32dc8f8308492855fe0e9de031
ginz1403 <96443195+ginz1403@users.noreply.github.com>
2022-09-24 11:21:37 +0700

    thay doi
    123

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('simplesale', 'root', 'qwerty@123456', {
+const sequelize = new Sequelize('simplesale', 'root', null, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
